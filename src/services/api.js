b77dbba48e7abd79a1f94486fc1d741219273b9a
Mr-Jahanzaib-dev <jahanzaibkhalil48@gmail.com>
2025-12-18 10:30:57 +0500

    Update API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL;
-
-
-if(!API_URL) {
-  throw new Error('REACT_APP_API_URL is not defined in environment variables');
-}
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 const api = axios.create({
   baseURL: API_URL,
